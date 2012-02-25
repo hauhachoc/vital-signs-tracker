3f6e9a55e51f3247e789a53a31a50d4aa773bd48
kelvin <kvk168@gmail.com>
2012-02-24 16:21:56 -0800

    first commit

---
+++ csc780/src/com/vitalsigntracker/android/Csc780Activity.java
@@ -0,0 +1,13 @@
+package com.vitalsigntracker.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Csc780Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
