cda8c1d29eb73bbd7e60066762e044fa8c315a9f
Jayant <Jayant@Coldlair>
2012-05-15 23:35:20 -0700

    Weka exploration

---
+++ WekaStudy/src/com/WekaStudy/StatisticCollection.java
@@ -0,0 +1,51 @@
+package com.WekaStudy;
+
+public class StatisticCollection {
+	
+	    private Double classID;
+	    private int count;
+	    private String label;
+	    private Double percentage; 
+	    
+	    /*getters*/
+	    Double getClassID(){
+	         return this.classID;
+	    }
+
+	    Double getPercentage(){
+	         return this.percentage;
+	    }
+	    
+	    String getLabel(){
+	         return this.label;
+	    }
+	    
+	    int getCount(){
+	         return this.count;
+	    }
+	    
+	    /*setters*/
+	    
+	    void setClassID(Double _classID){
+	         this.classID = _classID;
+	    }
+
+	    void setPercentage(Double _percentage){
+	         this.percentage = _percentage;
+	    }
+	    
+	    void setLabel(String _label){
+	         this.label = _label;
+	    }
+
+	    void setClassID(int _count){
+	         this.count = _count;
+	    }
+
+	    void incrementCount(){
+	         this.count++;
+	    }
+	    
+	}
+
+
